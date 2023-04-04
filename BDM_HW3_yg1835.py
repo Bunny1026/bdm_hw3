2e8908cc32dd9e7f12a1e36785b46ebb1b6ca826
Bunny1026 <57078192+Bunny1026@users.noreply.github.com>
2023-04-04 16:05:00 -0400

    Add files via upload

---
+++ BDM_HW3_yg1835.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+import csv
+
+sc = pyspark.SparkContext.getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+
+C = sc.textFile('/shared/CUSP-GX-6002/data/complaints.csv', use_unicode=True).cache()
+header = C.first().split(',')
+
+def extractFeatures(partId, rows):
+    if partId == 0:
+      next(rows)
+    reader = csv.reader(rows)
+    for row in reader:
+      if len(row) == 18 and len(row[0])==10:
+        product, date, company,comp_id = row[1],row[0],row[7],row[17]
+        year = date.strip().split("-")[0]
+        yield(product, year, company,comp_id,1)
+
+D = C.mapPartitionsWithIndex(extractFeatures)
+D1 = D.map(lambda x: (x[0].strip().lower(),x[1],x[2].strip().lower()))      .distinct()      .map(lambda x: ((x[0],x[1]),1))      .reduceByKey(lambda x,y: x+y)      .sortBy(lambda x: (x[0][0],x[0][1]))
+
+
+D11 = D.map(lambda x: ((x[0].strip().lower(),x[1],x[2].strip().lower()),1))      .reduceByKey(lambda x,y: x+y)      .filter(lambda x: x[1]>0)      .map(lambda x: ((x[0][0],x[0][1]),1))      .reduceByKey(lambda x,y: x+y)      .sortBy(lambda x: (x[0][0],x[0][1]))
+
+D2 = D.map(lambda x: ((x[0].strip().lower(),x[1]),1))      .reduceByKey(lambda x,y: x+y)      .sortBy(lambda x: (x[0][0],x[0][1]))      .join(D11)
+
+# get the total number of complaints received per product, per year
+D3 = D.map(lambda x: ((x[0].strip().lower(),x[1]),1))      .reduceByKey(lambda x,y: x+y)      .sortBy(lambda x: (x[0][0],x[0][1]))
+D3.take(5)
+# get the percentage
+D4 = D.map(lambda x: ((x[0].strip().lower(),x[1],x[2].strip().lower()),1))      .reduceByKey(lambda x,y: x+y)      .map(lambda x: ((x[0][0],x[0][1]),(x[0][2],x[1])))      .groupByKey().mapValues(lambda x: max(x,key=lambda y:y[1]))      .join(D3)      .sortBy(lambda x: (x[0][0],x[0][1]))      .map(lambda x: ((x[0][0],x[0][1]),round(x[1][0][1]/x[1][1]*100)))
+      
+outputTask1 = D4.join(D2)                .map(lambda x: (x[0][0],x[0][1],x[1][1][0],x[1][1][1],x[1][0]))                .sortBy(lambda x: (x[0],x[1]))                .map(lambda x: ','.join(map(str, x)))
+
+print(outputTask1.take(10).collect())
+            
+
